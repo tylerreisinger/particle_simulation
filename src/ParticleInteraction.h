8c7cb157fd955df801aef332d54666a76f1b3ab0
tylerreisinger <reisinger.tyler@gmail.com>
2016-12-02 18:31:43 -0500

    Removed old ParticleInteraction class, replaced with a simple interface

--- src/ParticleInteraction.h
+++ src/ParticleInteraction.h
@@ -1,48 +1,16 @@
 #ifndef PS_PARTICLEINTEREACTION_H_
 #define PS_PARTICLEINTEREACTION_H_
 
-#include <vector>
-#include <unordered_map>
-#include <memory>
-
-#include "InteractionComponent.h"
 #include "CommonTypes.h"
-#include "Particle.h"
 
-class ParticleInteraction {
+class Particle;
+
+class IParticleInteraction {
 public:
-    ParticleInteraction(const Particle* particle = nullptr);
-    ~ParticleInteraction() = default;
+    IParticleInteraction() = default;
+    virtual ~IParticleInteraction() {};
 
-    ParticleInteraction(const ParticleInteraction& other) = delete;
-    ParticleInteraction(ParticleInteraction&& other) noexcept = default;
-    ParticleInteraction& operator =(const ParticleInteraction& other) = delete;
-    ParticleInteraction& operator =(ParticleInteraction&& other) noexcept = default;
-
-    void add_interaction_component(std::unique_ptr<InteractionComponent> component) {
-    }
-
-    ForceType compute_force(const Particle& target, const Particle& source) {
-        ForceType force = 0;
-        for(std::size_t i = 0; i < m_components.size(); ++i) {
-            force += m_components[i]->compute_force(target, source);
-        }
-        return force;
-    }
-
-    ParticleInteraction clone(const Particle* parent) const {
-        ParticleInteraction clone(parent);
-
-        for(auto& component : m_components) {
-            clone.add_interaction_component(component->clone());
-        }
-
-        return clone;
-    }
-
-private: 
-    std::vector<ChargeType> m_charges;
-    std::vector<std::unique_ptr<InteractionComponent>> m_components;
+    virtual ForceType compute_force(const Particle& other) = 0;
 };
 
 #endif
